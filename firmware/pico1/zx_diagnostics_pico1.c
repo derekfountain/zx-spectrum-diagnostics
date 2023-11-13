953a885c3ee13ec8f4e487756e0c2336d8764c6a
Derek Fountain <derek@scratters.com>
2023-11-13 16:30:22 +0000

    build a bit of framework

--- firmware/pico1/zx_diagnostics_pico1.c
+++ firmware/pico1/zx_diagnostics_pico1.c
@@ -2,7 +2,12 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 
+#include "page.h"
 #include "oled.h"
+
+#include "page_voltages.h"
+
+static page_t *current_page;
 
 void main( void )
 {
@@ -13,19 +18,26 @@ void main( void )
   /* Initialise OLED screen with default font */
   init_oled( NULL );
   
-  draw_str(4,   0,  "Spectrum Diagnostics");
-  draw_char(0,   56, 'X');
-  draw_char(123, 56, 'X');
+  current_page = get_voltage_page();
+  (current_page->entry_fn)();
 
-  uint16_t x;
-  for( x=0; x<128; x++ )
-    draw_pixel( x, 10 );
-
-  update_screen();
-
-  while (1)
+  while( 1 )
   {
-    sleep_ms(1000);
+    // This can't take too long. If it turns out that tests need to
+    // run a long time, this will have to be done on the second core
+    //
+    (current_page->tests_fn)();
+
+    for( uint32_t i=0; i < current_page->repeat_pause_ms; i++ )
+    {
+      sleep_ms( 1 );
+
+      // Sample button, if pressed change current_page to next
+      // Run exit function
+      // Update pointer
+      // Run entry function
+      break;
+    }
   }
 
 }
