6dd853b68ec488a78e85cf1c81f5b62cb9591b21
Derek Fountain <derek@scratters.com>
2024-02-17 19:09:58 +0000

    better dbus test result, try to reflect stuck lines

--- firmware/pico1/page_dbus.c
+++ firmware/pico1/page_dbus.c
@@ -14,9 +14,9 @@
 #define TEST_TIME_SECS   2
 #define TEST_TIME_SECS_F ((float)(TEST_TIME_SECS))
 
-#define NUM_DBUS_TESTS 1
+#define NUM_DBUS_TEST_RESULT_LINES 3
 #define WIDTH_OLED_CHARS 32
-static uint8_t result_line_txt[NUM_DBUS_TESTS][WIDTH_OLED_CHARS+1];
+static uint8_t result_line_txt[NUM_DBUS_TEST_RESULT_LINES][WIDTH_OLED_CHARS+1];
 
 static SEEN_EDGE d0_flag   = SEEN_NEITHER;
 static SEEN_EDGE d1_flag   = SEEN_NEITHER;
@@ -53,14 +53,14 @@ void dbus_page_entry( void )
   /* Need to hold the Z80 offline while I set these up or they fire too early */
   gpio_put( GPIO_Z80_RESET, 1 ); sleep_ms( 650 );
 
-  d0_flag   = SEEN_NEITHER;
-  d1_flag   = SEEN_NEITHER;
-  d2_flag   = SEEN_NEITHER;
-  d3_flag   = SEEN_NEITHER;
-  d4_flag   = SEEN_NEITHER;
-  d5_flag   = SEEN_NEITHER;
-  d6_flag   = SEEN_NEITHER;
-  d7_flag   = SEEN_NEITHER;
+  d0_flag = SEEN_NEITHER;
+  d1_flag = SEEN_NEITHER;
+  d2_flag = SEEN_NEITHER;
+  d3_flag = SEEN_NEITHER;
+  d4_flag = SEEN_NEITHER;
+  d5_flag = SEEN_NEITHER;
+  d6_flag = SEEN_NEITHER;
+  d7_flag = SEEN_NEITHER;
 
   gpio_set_irq_enabled( GPIO_DBUS_D0, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, true );
   gpio_set_irq_enabled( GPIO_DBUS_D1, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, true );
@@ -83,15 +83,46 @@ void dbus_page_exit( void )
   gpio_set_irq_enabled( GPIO_DBUS_D6, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, false );
   gpio_set_irq_enabled( GPIO_DBUS_D7, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, false );
 
-  snprintf( result_line_txt[0], WIDTH_OLED_CHARS, "Present: %c%c%c%c%c%c%c%c",
-	    d7_flag == SEEN_BOTH ? '7' : 'x',
-	    d6_flag == SEEN_BOTH ? '6' : 'x',
-	    d5_flag == SEEN_BOTH ? '5' : 'x',
-	    d4_flag == SEEN_BOTH ? '4' : 'x',
-	    d3_flag == SEEN_BOTH ? '3' : 'x',
-	    d2_flag == SEEN_BOTH ? '2' : 'x',
-	    d1_flag == SEEN_BOTH ? '1' : 'x',
-	    d0_flag == SEEN_BOTH ? '0' : 'x');
+  snprintf( result_line_txt[0], WIDTH_OLED_CHARS, "        76543210" );
+  snprintf( result_line_txt[1], WIDTH_OLED_CHARS, "        --------" );
+  if( (d0_flag == SEEN_BOTH) &&
+      (d1_flag == SEEN_BOTH) &&
+      (d2_flag == SEEN_BOTH) &&
+      (d3_flag == SEEN_BOTH) &&
+      (d4_flag == SEEN_BOTH) &&
+      (d5_flag == SEEN_BOTH) &&
+      (d6_flag == SEEN_BOTH) &&
+      (d7_flag == SEEN_BOTH) )
+  {
+    /* This would be the norm: transitions low to high and high to low have both been seen */
+    snprintf( result_line_txt[2], WIDTH_OLED_CHARS, "Active: YYYYYYYY");
+  }
+  else
+  {
+    /*
+     * Logic is as follows: if flag is SEEN_NEITHER then there has been no edge is
+     * either direction. The line is stuck in whatever state it's currently in, high
+     * or low. So just read the GPIO state - that's how it's stuck.
+     *
+     * If the flag indicates the line has been seen going high, but not seen going
+     * low, then it must be stuck in its current state which is high.
+     *
+     * If the flag indicates the line has been seen going low, but not seen going
+     * high, then it must be stuck in its current state which is low.
+     *
+     * Whichever it is, the current state of the GPIO tells me whether it's stuck
+     * high or low.
+     */
+    snprintf( result_line_txt[2], WIDTH_OLED_CHARS, " Stuck: %c%c%c%c%c%c%c%c",
+	      d7_flag == SEEN_BOTH ? '.' : gpio_get( GPIO_DBUS_D7 ) ? 'H' : 'L',
+	      d6_flag == SEEN_BOTH ? '.' : gpio_get( GPIO_DBUS_D6 ) ? 'H' : 'L',
+	      d5_flag == SEEN_BOTH ? '.' : gpio_get( GPIO_DBUS_D5 ) ? 'H' : 'L',
+	      d4_flag == SEEN_BOTH ? '.' : gpio_get( GPIO_DBUS_D4 ) ? 'H' : 'L',
+	      d3_flag == SEEN_BOTH ? '.' : gpio_get( GPIO_DBUS_D3 ) ? 'H' : 'L',
+	      d2_flag == SEEN_BOTH ? '.' : gpio_get( GPIO_DBUS_D2 ) ? 'H' : 'L',
+	      d1_flag == SEEN_BOTH ? '.' : gpio_get( GPIO_DBUS_D1 ) ? 'H' : 'L',
+	      d0_flag == SEEN_BOTH ? '.' : gpio_get( GPIO_DBUS_D0 ) ? 'H' : 'L');
+  }
 }
 
 void dbus_page_gpios( uint32_t gpio, uint32_t events )
@@ -198,7 +229,7 @@ void dbus_page_run_tests( void )
 void dbus_output(void)
 {
   uint8_t line=2;
-  for( uint32_t test_index=0; test_index<NUM_DBUS_TESTS; test_index++ )
+  for( uint32_t test_index=0; test_index<NUM_DBUS_TEST_RESULT_LINES; test_index++ )
   {      
     draw_str(0, line*8, "                         " );
     draw_str(0, line*8, result_line_txt[test_index] );      
