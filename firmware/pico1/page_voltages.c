43c2d90da9745f4f6075ac46dfab83a6a9f1245b
Derek Fountain <derek@scratters.com>
2024-03-11 17:59:56 +0000

    rework the code to handle the rejigged -5V voltage divider

--- firmware/pico1/page_voltages.c
+++ firmware/pico1/page_voltages.c
@@ -69,10 +69,9 @@ void voltage_page_exit( void )
 }
 
 /*
- * These are all 1K5 except R105 on the 12V supply which is 56K.
  * I measured mine out of circuit before soldering them
  */
-#define R101 1498.0
+#define R101  392.0
 #define R102 1498.0
 #define R103 1496.0
 #define R104 1496.0
@@ -80,6 +79,14 @@ void voltage_page_exit( void )
 #define R106 1496.0
 
 /*
+ * I rejigged the -5V voltage divider so it went between +3V3 and -5V instead
+ * of +5V and -5V which was the original idea. I used schottky diodes instead
+ * of 1N4148s for the clamping because I needed to ensure no lower than -0.5V
+ * went into the ADC (the lower VF of the schottky's ensures this). Result was
+ * I had to rework out how to get the correct voltage shown on the OLED. I'm
+ * not quite sure how I ended up working out how to do it, so I've ended up
+ * with the -5V display being calculated differently to the other two.
+ * 
  * Some worked examples from my test Spectrum:
  *
  *  +5V on the edge connector is  5.03V
@@ -88,21 +95,27 @@ void voltage_page_exit( void )
  *
  *  5V_DIVIDED on the board's test point is 2.52V, should be 2.515V
  * 12V_DIVIDED on the board's test point is 2.61V, should be 2.577V
- * -5V_DIVIDED on the board's test point is 3.14V, should be 3.79/2 = 1.895V
- *   (so that -5V one is over 500mV off)
+ *
+ * For -5V:
+ *
+ * 3.30 - 1.2400 = 2.06 dropped across whole divider
+ * 2.06 * 0.2075 = 0.4275V dropped across R1 (see _minus5_ratio below)
+ *
+ * So 3.3 - 0.4275 = 2.8725V at the divider's sample point
+ *
+ * -5V_DIVIDED on the board's test point is 2.84V, should be 2.8725V
  *
  *  +5V ADC value is approx 3210
  * +12V ADC value is approx 3320 (variable, goes to over 3400 sometimes)
- *  -5V ADC value is approx 3995 (variable, goes over 4000 quite frequently)
+ *  -5V ADC value is approx 3580 (variable, goes over 4000 quite frequently)
  *
  *  +5V calculation is (3210 * 0.00081) / (1.0 - 0.5000) =  5.20V, shown value is  5.16V
  * +12V calculation is (3320 * 0.00081) / (1.0 - 0.7906) = 12.84V, shown value is 12.75V
- *  -5V calculation is (3995 * 0.00081) / (1.0 - 0.5001) - 5.00 = 1.473V shown value is 1.40V
+ *  -5V calculation is 3.3 - (3.3 - ((3580 * 0.00081)) / 0.2075) = 1.371V, shown value is 1.30V
  *
- * So +5V is 130mV off, which is mostly in the ADC conversion. +12V is close, but
+ * So +5V is 60mV off, which is mostly in the ADC conversion. +12V is close, but
  * it's a wobbly voltage at the best of times because of the way it's created.
- * On this board, -5V is broken, so I'm not sure what to trust. The voltage divider
- * seems to be putting out a weird value, but it's hard to tell what's a fault.
+ * On this board, -5V is broken, so I'm not sure what to trust.
  *
  * I tried a board with a valid -5V supply and the OLED report said -4.47ish, which
  * is exactly what I'd expect. So I think it's OK.
@@ -111,7 +124,7 @@ const float conversion_factor = 3.3f / (1 << 12);          // 0.00081
 
 const float _5v_ratio     = 1.0 - (R103 / (R103 + R104));  // 0.50000 on my test board
 const float _12v_ratio    = 1.0 - (R105 / (R105 + R106));  // 0.20935 on my test board
-const float _minus5_ratio = 1.0 - (R101 / (R101 + R102));  // 0.49997 on my test board
+const float _minus5_ratio =       (R101 / (R101 + R102));  // 0.20750 on my test board
 
 void voltage_page_test_5v( void )
 {
@@ -136,8 +149,21 @@ void voltage_page_test_12v( void )
 void voltage_page_test_minus5v( void )
 {
   adc_select_input( 0 );
-  float reading = ((adc_read() * conversion_factor) / _minus5_ratio) - 5.00;
-  // float reading = adc_read();
+
+  /*
+   * Samples from working -5V supply showing -5.4 on the meter:
+   *
+   * adc_read()     = 1860.0
+   * 1860 * 0.00081 = 1.50    this is the voltage at the sample point
+   * 
+   * Drop across R1 is therefore 3.3 - 1.50 = 1.800V
+   * 1.8000 / 0.20750 (min5 voltage divider ratio) gives drop across
+   *                  entire voltage divider = 8.6746
+   * 3.3 - 8.6746 = -5.3746
+   * 
+   */
+
+  float reading = 3.3000 - ((3.3000 - (adc_read() * conversion_factor)) / _minus5_ratio);
   snprintf( result_line_txt[2], WIDTH_OLED_CHARS, "    -5V: %0.3fV", reading );
 
   average_min5v[average_index] = reading;
